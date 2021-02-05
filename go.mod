e1f094a06b757690ef65790d601cab7155ccf0ac
Pavel Sivashchenko <pavel.sivashchenko@mail.ru>
2021-02-05 13:28:04 +0300

    removed json validation

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/Maxkile/jsschema
+
+go 1.15
+
+require (
+	github.com/lestrrat-go/jspointer v0.0.0-20181205001929-82fadba7561c // indirect
+	github.com/lestrrat-go/jsref v0.0.0-20181205001954-1b590508f37d
+	github.com/lestrrat-go/jsval v0.0.0-20181205002323-20277e9befc0 // indirect
+	github.com/lestrrat-go/pdebug v0.0.0-20210111095411-35b07dbf089b
+	github.com/lestrrat-go/structinfo v0.0.0-20190212233437-acd51874663b // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.7.0
+	github.com/xeipuuv/gojsonschema v1.2.0
+)
--- schema_test.go
+++ schema_test.go
@@ -1,15 +1,12 @@
 package schema_test
 
 import (
-	"encoding/json"
-	"fmt"
+	schema "github.com/Maxkile/jsschema"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/lestrrat-go/jsschema"
-	"github.com/lestrrat-go/jsschema/validator"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,78 +27,6 @@ func readSchema(f string) (*schema.Schema, error) {
 		return nil, err
 	}
 	return schema.Read(in)
-}
-
-func TestValidate(t *testing.T) {
-	tests := []string{
-		"allof",
-		"anyof",
-		"array",
-		"arraylength",
-		"arraytuple",
-		"arraytuple_disallow_additional",
-		"arrayunique",
-		"boolean",
-		"business",
-		"integer",
-		"not",
-		"null",
-		"numrange",
-		"numrange_exclmax",
-		"objectpatterns",
-		"objectpropdepend",
-		"objectpropsize",
-		"objectproprequired",
-		"oneof",
-		"strlen",
-		"strpattern",
-	}
-	for _, name := range tests {
-		schemaf := filepath.Join("test", name+".json")
-		t.Logf("Reading schema file %s", schemaf)
-		schema, err := readSchema(schemaf)
-		if !assert.NoError(t, err, "reading schema file %s should succeed", schemaf) {
-			return
-		}
-
-		valid := validator.New(schema)
-
-		pat := filepath.Join("test", fmt.Sprintf("%s_pass*.json", name))
-		files, _ := filepath.Glob(pat)
-		for _, passf := range files {
-			t.Logf("Testing schema against %s (expect to PASS)", passf)
-			passin, err := os.Open(passf)
-			if !assert.NoError(t, err, "os.Open(%s) should succeed", passf) {
-				return
-			}
-			var m map[string]interface{} // XXX should test against structs
-			if !assert.NoError(t, json.NewDecoder(passin).Decode(&m), "json.Decode should succeed") {
-				return
-			}
-
-			if !assert.NoError(t, valid.Validate(m), "schema.Validate should succeed") {
-				return
-			}
-		}
-
-		pat = filepath.Join("test", fmt.Sprintf("%s_fail*.json", name))
-		files, _ = filepath.Glob(pat)
-		for _, failf := range files {
-			t.Logf("Testing schema against %s (expect to FAIL)", failf)
-			failin, err := os.Open(failf)
-			if !assert.NoError(t, err, "os.Open(%s) should succeed", failf) {
-				return
-			}
-			var m map[string]interface{} // XXX should test against structs
-			if !assert.NoError(t, json.NewDecoder(failin).Decode(&m), "json.Decode should succeed") {
-				return
-			}
-
-			if !assert.Error(t, valid.Validate(m), "schema.Validate should fail") {
-				return
-			}
-		}
-	}
 }
 
 func TestExtras(t *testing.T) {
